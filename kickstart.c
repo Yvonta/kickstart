8aaa0c64a9f662f13dc5be1453956a74debab52e
Dirk Jan Buter <44683987+dirkjanbuter@users.noreply.github.com>
2026-08-13 10:28:24 +0200

    wider

--- kickstart.c
+++ kickstart.c
@@ -3,11 +3,14 @@
 // macOS Implementation (Pure Objective-C)
 // ==========================================
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
 #import <stdlib.h>
 #import <CoreGraphics/CoreGraphics.h>
 
-#define WINDOW_WIDTH 425
-#define WINDOW_HEIGHT 40
+#define WINDOW_WIDTH 468
+#define TOOLBAR_HEIGHT 50
+#define AD_HEIGHT 90
+#define WINDOW_HEIGHT (TOOLBAR_HEIGHT + AD_HEIGHT)
 
 @interface TransparentPanel : NSPanel
 @end
@@ -15,36 +18,71 @@
 @implementation TransparentPanel
 - (BOOL)canBecomeKeyWindow { return YES; }
 - (BOOL)canBecomeMainWindow { return YES; }
+- (BOOL)acceptsFirstMouse:(NSEvent *)event { return YES; }
 @end
 
-@interface AppDelegate : NSObject <NSApplicationDelegate> {
+@interface AppDelegate : NSObject <NSApplicationDelegate, WKUIDelegate, WKNavigationDelegate> {
     TransparentPanel *window;
     NSTextField *textInput;
+    WKWebView *webView;
 }
 @end
 
 @implementation AppDelegate
+
 - (void)submitQuery:(id)sender {
     NSString *text = [textInput stringValue];
     if (text == nil || [text length] == 0) return;
+    
     NSString *encoded = [text stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     if (encoded) {
-        NSString *url = [NSString stringWithFormat:@"open https://yvonta.ai/apppub/automation/ask.php?q=%@", encoded];
-        system([url UTF8String]);
+        NSString *urlString = [NSString stringWithFormat:@"https://yvonta.ai/apppub/automation/ask.php?q=%@", encoded];
+        NSURL *url = [NSURL URLWithString:urlString];
+        if (url) {
+            [[NSWorkspace sharedWorkspace] openURL:url];
+        }
     }
 }
-- (void)openUrl:(id)sender { system("open https://yvonta.com/"); }
+
+- (void)openUrl:(id)sender { 
+    NSURL *url = [NSURL URLWithString:@"https://yvonta.com"];
+    if (url) {
+        [[NSWorkspace sharedWorkspace] openURL:url];
+    }
+}
+
+// --- WKNavigationDelegate: Vang normale links af en open ze extern ---
+- (void)webView:(WKWebView *)view decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    NSURL *url = navigationAction.request.URL;
+    
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated || 
+        (navigationAction.targetFrame == nil && url != nil)) {
+        [[NSWorkspace sharedWorkspace] openURL:url];
+        decisionHandler(WKNavigationActionPolicyCancel);
+        return;
+    }
+    
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+// --- WKUIDelegate: Vang target="_blank" en window.open pop-ups af en open ze extern ---
+- (WKWebView *)webView:(WKWebView *)view createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
+    NSURL *url = navigationAction.request.URL;
+    if (url != nil) {
+        [[NSWorkspace sharedWorkspace] openURL:url];
+    }
+    return nil;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification * _Nonnull)aNotification {
     NSRect screenFrame = [[NSScreen mainScreen] frame];
-    NSRect rect = NSMakeRect((screenFrame.size.width - WINDOW_WIDTH)/2, screenFrame.size.height - 70, WINDOW_WIDTH, WINDOW_HEIGHT);
+    NSRect rect = NSMakeRect((screenFrame.size.width - WINDOW_WIDTH)/2, screenFrame.size.height - 160, WINDOW_WIDTH, WINDOW_HEIGHT);
     
     window = [[TransparentPanel alloc] initWithContentRect:rect
                                                  styleMask:NSWindowStyleMaskBorderless
                                                    backing:NSBackingStoreBuffered
                                                      defer:NO];
     
-    // Use normal window level with stationary behavior to sit on the desktop layer interactively
     [window setLevel:NSNormalWindowLevel];
     [window setCollectionBehavior:NSWindowCollectionBehaviorStationary | NSWindowCollectionBehaviorCanJoinAllSpaces];
     [window setOpaque:NO];
@@ -54,33 +92,28 @@
     [window setIgnoresMouseEvents:NO];
     [window makeKeyAndOrderFront:nil];
 
-    // Root content view with explicit alpha 0.0 clear background
     NSView *content = [window contentView];
     [content setWantsLayer:YES];
     [content.layer setBackgroundColor:[NSColor colorWithCalibratedRed:0.0 green:0.0 blue:0.0 alpha:0.0].CGColor];
     
-    // Floating toolbar container with alpha 0.0 background
-    NSView *container = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)];
+    // --- Toolbar Container ---
+    NSView *container = [[NSView alloc] initWithFrame:NSMakeRect(0, AD_HEIGHT, WINDOW_WIDTH, TOOLBAR_HEIGHT)];
     [container setWantsLayer:YES];
     [container.layer setBackgroundColor:[NSColor colorWithCalibratedRed:0.0 green:0.0 blue:0.0 alpha:0.0].CGColor];
     [container.layer setCornerRadius:8.0];
     [content addSubview:container];
     
-    // --- Logo Button ---
+    // Logo Button
     NSButton *logoButton = [[NSButton alloc] initWithFrame:NSMakeRect(5, 5, 30, 30)];
     [logoButton setButtonType:NSButtonTypeMomentaryChange];
     [logoButton setBordered:NO];
     [logoButton setTarget:self];
     [logoButton setAction:@selector(openUrl:)];
 
-    // Look for logo.png inside the app bundle's Resources directory
     NSString *logoPath = [[NSBundle mainBundle] pathForResource:@"logo" ofType:@"png"];
-    
-    // Fallback to current working directory if not running from a bundled *.app
     if (!logoPath) {
         logoPath = [[[NSFileManager defaultManager] currentDirectoryPath] stringByAppendingPathComponent:@"logo.png"];
     }
-    
     NSImage *logoImage = [[NSImage alloc] initWithContentsOfFile:logoPath];
 
     if (logoImage) {
@@ -92,21 +125,34 @@
     }
     [container addSubview:logoButton];
 
-    // --- Text Input Field ---
-    textInput = [[NSTextField alloc] initWithFrame:NSMakeRect(40, 8, 330, 24)];
+    // Text Input Field
+    textInput = [[NSTextField alloc] initWithFrame:NSMakeRect(40, 8, 373, 24)];
     [[textInput cell] setPlaceholderString:@"Ask Yvonta..."];
     [textInput setTarget:self];
     [textInput setAction:@selector(submitQuery:)];
     [container addSubview:textInput];
 
-    // --- GO! Button ---
-    NSButton *goButton = [[NSButton alloc] initWithFrame:NSMakeRect(378, 6, 40, 28)];
+    // GO! Button
+    NSButton *goButton = [[NSButton alloc] initWithFrame:NSMakeRect(421, 6, 40, 28)];
     [goButton setTitle:@"GO!"];
     [goButton setButtonType:NSButtonTypeMomentaryLight];
     [goButton setBezelStyle:NSBezelStyleRounded];
     [goButton setTarget:self];
     [goButton setAction:@selector(submitQuery:)];
     [container addSubview:goButton];
+
+    // --- Web Browser Ad Widget ---
+    WKWebViewConfiguration *webConfig = [[WKWebViewConfiguration alloc] init];
+    webView = [[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, WINDOW_WIDTH, AD_HEIGHT) configuration:webConfig];
+    [webView setValue:@YES forKey:@"drawsBackground"];
+    
+    webView.navigationDelegate = self;
+    webView.UIDelegate = self;
+
+    NSURL *adURL = [NSURL URLWithString:@"https://yvonta.ai/apppub/banners/kickstart.html"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:adURL];
+    [webView loadRequest:request];
+    [content addSubview:webView];
 }
 @end
 
@@ -121,109 +167,4 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-#else
-// ==========================================
-// Linux / Manjaro Implementation (Pure GTK3)
-// ==========================================
-#include <gtk/gtk.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define WINDOW_WIDTH 425
-#define WINDOW_HEIGHT 40
-
-typedef struct {
-    GtkWidget *text_input;
-} AppElements;
-
-static void on_submit(GtkWidget *widget, gpointer data) {
-    AppElements *app = (AppElements *)data;
-    const gchar *text = gtk_entry_get_text(GTK_ENTRY(app->text_input));
-    if (text == NULL || strlen(text) == 0) return;
-
-    gchar *escaped = g_uri_escape_string(text, NULL, FALSE);
-    gchar *url = g_strdup_printf("xdg-open 'https://yvonta.ai/apppub/automation/ask.php?q=%s'", escaped);
-    system(url);
-    g_free(escaped);
-    g_free(url);
-}
-
-static void on_logo_click(GtkWidget *widget, gpointer data) {
-    system("xdg-open 'https://yvonta.com/'");
-}
-
-static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-    gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
-    
-    gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DESKTOP);
-    gtk_window_set_keep_below(GTK_WINDOW(window), TRUE);
-    gtk_widget_set_app_paintable(window, TRUE);
-
-    GdkDisplay *display = gdk_display_get_default();
-    GdkMonitor *monitor = gdk_display_get_primary_monitor(display);
-    if (!monitor) monitor = gdk_display_get_monitor(display, 0);
-    
-    GdkRectangle geometry;
-    gdk_monitor_get_geometry(monitor, &geometry);
-    gtk_window_move(GTK_WINDOW(window), geometry.x + (geometry.width - WINDOW_WIDTH) / 2, geometry.y + 30);
-
-    GdkScreen *screen = gtk_widget_get_screen(window);
-    GdkVisual *visual = gdk_screen_get_rgba_visual(screen);
-    if (visual && gdk_screen_is_composited(screen)) {
-        gtk_widget_set_visual(window, visual);
-    }
-
-    GtkCssProvider *css = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(css, "window, window.background { background-color: rgba(0,0,0,0.0); background-image: none; } .box { background-color: rgba(0,0,0,0.0); border-radius: 8px; padding: 5px; }", -1, NULL);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 8);
-    gtk_style_context_add_class(gtk_widget_get_style_context(box), "box");
-    gtk_container_add(GTK_CONTAINER(window), box);
-
-    // Logo Button
-    GtkWidget *logo_button = gtk_button_new();
-    gtk_widget_set_size_request(logo_button, 40, 30);
-    gtk_button_set_relief(GTK_BUTTON(logo_button), GTK_RELIEF_NONE);
-    
-    GError *error = NULL;
-    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale("logo.png", 24, 24, TRUE, &error);
-    if (pixbuf) {
-        GtkWidget *image = gtk_image_new_from_pixbuf(pixbuf);
-        gtk_container_add(GTK_CONTAINER(logo_button), image);
-        g_object_unref(pixbuf);
-    } else {
-        gtk_button_set_label(GTK_BUTTON(logo_button), "Y");
-        if (error) g_error_free(error);
-    }
-    
-    g_signal_connect(logo_button, "clicked", G_CALLBACK(on_logo_click), NULL);
-    gtk_box_pack_start(GTK_BOX(box), logo_button, FALSE, FALSE, 0);
-
-    // Text Input Field
-    AppElements *elements = g_new(AppElements, 1);
-    elements->text_input = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(elements->text_input), "Ask Yvonta...");
-    g_signal_connect(elements->text_input, "activate", G_CALLBACK(on_submit), elements);
-    gtk_box_pack_start(GTK_BOX(box), elements->text_input, TRUE, TRUE, 0);
-
-    // GO! Button
-    GtkWidget *go_button = gtk_button_new_with_label("GO!");
-    gtk_widget_set_size_request(go_button, 40, 30);
-    g_signal_connect(go_button, "clicked", G_CALLBACK(on_submit), elements);
-    gtk_box_pack_start(GTK_BOX(box), go_button, FALSE, FALSE, 0);
-
-    gtk_widget_show_all(window);
-}
-
-int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.yvonta.kickstart", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-    return status;
-}
 #endif
